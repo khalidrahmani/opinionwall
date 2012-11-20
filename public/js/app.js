7951f584b79939a3f07b04ded29cab877edcd968
Khalid Rahmani <khalid.rahmani.mail@gmail.com>
2012-11-20 22:34:49 +0000

    design Ok

--- public/js/app.js
+++ public/js/app.js
@@ -1,41 +1,18 @@
 $(document).ready(function () {
 
-  $('.confirm').submit(function (e) {
-    e.preventDefault();
-    var self = this;
-    var msg = 'Are you sure?';
-    bootbox.confirm(msg, 'cancel', 'Yes! I am sure', function (action) {
-      if (action) {
-        $(self).unbind('submit');
-        $(self).trigger('submit');
-      }
-    });
-  });
-  var i = $("#hidden_input").val() 
-  $("#add_choice").click(function() {
-	  $('#choices').append('<p> <input type="text" name="survey[choices]['+i+'][_id]"><a class="btn remove_choice" href="#"> - </a> </p>');
+  var i =  parseInt($("#hidden_input").val()) 
+  $("#add_choice").click(function(e) {
+	  e.preventDefault()	  
+	  $('#choices').append('<div class="control-group"><label class="control-label required">'+(i+1)+'</label><div class="controls"><input type="text" value="" name="survey[choices]['+i+'][_id]" tabindex="1"> <a class="btn remove_choice" href="#"> x </a> </div></div> ');
 	  i++
 	});    
   
   $(".remove_choice").live("click", function() {
-	  $(this).closest('p').remove();	 
+	  $(this).closest('div .control-group').remove();	 
 	  $("#choices input:text").each(function(index) {
 		    $(this).attr("name", "survey[choices]["+index+"][_id]")
 		    i = index+1
 	  });
 
-	});    
-  
-/* 
-$('#login').submit(function (e) {
-		e.preventDefault()
-		$.getJSON("/search",{q: $("#q").val(), page : page},
-            function(data) {
-                $(".surveys").append(data.html)
-                $(".ajax").html("Load more results ...")
-                page++
-        })
-		
-		alert("login submitted")  
-})*/
+	});
 })
